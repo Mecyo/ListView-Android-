ab59a51556e9d426248f01bbb3364115033abdcf
Mecyo <ejsmao@hotmail.com>
2018-07-19 18:25:27 -0300

    ListView com acesso a base de dados

---
+++ app/src/main/java/Util/Uteis.java
@@ -0,0 +1,24 @@
+package Util;
+
+/**
+ * Created by Emerson Santos on 19/07/2018.
+ */
+
+import android.app.AlertDialog;
+import android.content.Context;
+import com.example.aluno.listview.R;
+
+
+public class Uteis {
+    public static void Alert(Context context, String mensagem){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+//ADICIONANDO UM TITULO A NOSSA MENSAGEM DE ALERTA
+        alertDialog.setTitle(R.string.app_name);
+//MENSAGEM A SER EXIBIDA
+        alertDialog.setMessage(mensagem);
+//CRIA UM BOTÃO COM O TEXTO OK SEM AÇÃO
+        alertDialog.setPositiveButton("OK", null);
+//MOSTRA A MENSAGEM NA TELA
+        alertDialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/aluno/listview/ListarPessoas.java
@@ -0,0 +1,13 @@
+package com.example.aluno.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListarPessoas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_pessoas);
+    }
+}
